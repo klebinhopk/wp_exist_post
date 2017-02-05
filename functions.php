a97f1d9be7b7dd34060705964794ebc66215527b
lenivene <lenivene@msn.com>
2017-02-05 13:24:23 -0300

    Update! wp-exist-posts.php
    Return Post ID if post exists, 0 otherwise.

--- functions.php
+++ functions.php
@@ -1,34 +1,64 @@
 <?php
-	/**
-	 * Check Exist Post By Title
-	 * @Version 1.0.0
-	 * @global class $wpdb
-	 *
-	 * @str $title title name post
-	 * @str $author ID number of author post ( no required )
-	 * @str $post_type no required and default post or other
-	 *
-	 **/
-	function wp_exist_post( $title = '', $author = '', $post_type='post' ) {
-		global $wpdb;
-		if( empty( trim( $title ) ) )
-			return NULL;
+/**
+ * Check Exist Post By Title
+ * @Version 1.0.1
+ * @global class $wpdb
+ *
+ * @str $title title name post
+ * @str $author ID number of author post ( no required )
+ * @str $post_type no required and default post or other
+ *
+ **/
+	global $wpdb;
 
-		$title = urldecode( $title );
-
-		$sql  = "SELECT * FROM {$wpdb->posts} WHERE ";
-		if( !empty( $author ) ){
-			$arr[] = esc_sql( $author );
-			$sql .= "post_author = %d ";
-		}
-		$arr[] = esc_sql( $title );
-		$arr[] = esc_sql( $post_type );
-		$sql .= " AND post_title = %s AND post_type = %s";
-
-		$result = count( $wpdb->get_results( $wpdb->prepare( $sql, $arr ) ) );
-
-		if($result > 0)
-			return $result;
-
-		return NULL;
+	if( empty( $title ) ){
+		return 0;
 	}
+
+	$title = ( is_object( $title ) ) ? (array) $title : $title;
+
+	$query = "SELECT ID FROM $wpdb->posts WHERE 1=1";
+	$args  = array();
+
+	$error = $args;
+	$error_param = $args;
+
+
+	if( !empty( $title ) ){
+		$query .= " AND post_title = %s";
+		$args[] = $title;
+
+		if( is_array( $title ) || is_object( $title ) ){
+			$error_param[] = $title;
+			$error[] = __( 'Title required type string!', 'textdomain' );
+		}
+	}
+
+	if( !empty( $author ) ){
+		$query .= ' AND post_author = %d';
+		$args[] = $author;
+
+		if( !is_int( $author ) ){
+			$error_param[] = $author;
+			$error[] = __( 'Author ID required type int!', 'textdomain' );
+		}
+	}
+
+	if( !empty( $post_type ) ){
+		$query .= ' AND post_type = %s';
+		$args[] = $post_type;
+	}
+
+	/**
+	 * Instance error
+	 */
+	if( !empty( $error ) ){
+		return new WP_Error( 'wp_exist_post', $error, $error_param );
+	}
+
+	if( !empty( $args ) ){
+		return (int) $wpdb->get_var( $wpdb->prepare( $query, $args ) );
+	}
+
+	return 0;
+}
